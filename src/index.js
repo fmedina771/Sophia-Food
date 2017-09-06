c90052162b36d5c4a1b9ebd5ab830d083b4a1855
Fernando Medina <fmedina771@gmail.com>
2017-09-05 21:29:16 -0400

    create-react-app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Layout from './Layout';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-
-ReactDOM.render(<Layout />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
