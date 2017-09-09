7297db38ea4013a5471642bbd0454f2b1ac396ba
Admin <Admin@Eliss-MacBook-Air.local>
2017-09-09 14:15:30 -0400

    building index.html in public dir

--- src/layout.js
+++ src/layout.js
@@ -4,13 +4,10 @@ import './layout.css';
 export default class layout extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/layout.js</code> and save to reload.
-        </p>
+      <div className="TBD">
+{/*  <Results />
+    <Dashboard />
+    */} 
       </div>
     );
   }
