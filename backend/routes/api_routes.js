c620ed5c93fc0824d2dcae796c4549dd5005911c
Jane Lee <janelee@Janes-MacBook-Pro-2.local>
2017-09-09 14:51:42 -0400

    set up api routes

--- backend/routes/api_routes.js
+++ backend/routes/api_routes.js
@@ -9,3 +9,52 @@ var db = require("../models");
 
 // Routes
 // =============================================================
+module.exports = function(app) {
+    // Get requests
+    // GET route: "/api/users": display a JSON of all possible users
+    app.get('/api/users', function(req,res){
+        db.User.findAll({
+            data: user.uid
+        }).then(function(results){
+            res.json(results);
+            console.log(results);
+        });
+    });
+
+
+    // Post requests
+    // POST "/api/users" handles incoming survey results
+    app.post("/users", function(req, res, err) {
+
+        db.User.findAll({
+            where: {
+                uid: req.body.uid
+            }
+        }).then(
+            // Create the user
+            db.User.create({
+                displayName: req.body.username,
+                uid: req.body.uid,
+                email: req.body.email
+            }).catch(err => {
+                console.log({ err: err }, 'Unexpected Error')
+                throw err // Make sure cb gets the error
+            })
+        )
+    });
+
+
+    app.post("/questions", function(req, res) {
+        // update
+        db.User.update({
+            language: req.body.language,
+            rating: req.body.rating
+        })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            })
+        res.json(req.body);
+        // 1. Validate
+        // 2. Save via .create
+    })
+}
