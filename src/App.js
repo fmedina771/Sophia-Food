ba5d6b2064d9e05ab48256f1d2263684eace6278
Admin <Admin@Eliss-MacBook-Air.local>
2017-09-10 09:49:29 -0400

    start sign-in sign-up build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import './layout.css';
 
-export default class layout extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/layout.js</code> and save to reload.
-        </p>
+      <div className="TBD">
+{/*  <Results />
+    <Dashboard />
+    */} 
       </div>
     );
   }
